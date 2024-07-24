25a49a7741cd547beede9ea09ce7d9b0653f7a52
Ignatus <ignatusa3@gmail.com>
2024-07-24 13:19:31 +0000

    chore: update about and home page

--- textinfo.py
+++ textinfo.py
@@ -1,28 +1,43 @@
 aboutinfo = """
-       #### About Dataset
-The enormous amount of data produced in the context of the fourth industrial revolution frequently does not adequately depict African situations. 
-This disparity leads to a crucial problem: remedies drawn from such datasets frequently turn out to be ineffective when used to address problems unique to African regions. 
-As a result, questions concerning the effectiveness of technologies created without a thorough understanding of the distinctive features of the African landscape are becoming more and more prevalent.
+## About the Dataset
 
-An innovative project has been launched to close this gap—a dataset that is categorically "Afrocentric." 
-This unique dataset is carefully curated, concentrating only on data gathered from the various African areas. 
-This dataset's collection of annotated photos of leaves from diverse crops, showing both healthy specimens and those affected by illnesses, is one of its main components. 
-These pictures show the subtle symptoms of crop diseases at various phases of crop development.
+### RAIL Lab, KNUST
+The dataset from RAIL Lab, KNUST, plays a vital role in addressing the unique challenges of African agriculture. This project is centered on creating an "Afrocentric" dataset, specifically curated to reflect the agricultural diversity and challenges faced in various African regions.
 
-The dataset's emphasis on inclusion, which makes sure that it captures the agricultural diversity found in Africa, is essential to understanding its value. 
-The dataset offers a thorough understanding of disease patterns and manifestations by including annotated pictures of leaves from a range of crops. 
-Researchers, data scientists, and innovators who want to create specialized and efficient solutions for the agricultural problems the African continent faces will find this wealth of knowledge to be of immeasurable use.
+> **The enormous amount of data produced in the context of the fourth industrial revolution frequently does not adequately depict African situations. This disparity leads to a crucial problem: remedies drawn from such datasets frequently turn out to be ineffective when used to address problems unique to African regions. As a result, questions concerning the effectiveness of technologies created without a thorough understanding of the distinctive features of the African landscape are becoming more and more prevalent.**
 
-This "Afrocentric" dataset essentially serves as a testament to the understanding of the significance of region-specific data in promoting technological solutions that connect with the demands and complexities of Africa. 
-Datasets of this kind open the way for more informed and contextually relevant developments as we navigate the complex convergence of technology and agriculture, ensuring that the advantages of the fourth industrial revolution are realized inclusively across various global landscapes.
+> **An innovative project has been launched to close this gap—a dataset that is categorically "Afrocentric." This unique dataset is carefully curated, concentrating only on data gathered from the various African areas. This dataset's collection of annotated photos of leaves from diverse crops, showing both healthy specimens and those affected by illnesses, is one of its main components. These pictures show the subtle symptoms of crop diseases at various phases of crop development.**
 
-#### Content
+> **The dataset's emphasis on inclusion, which makes sure that it captures the agricultural diversity found in Africa, is essential to understanding its value. The dataset offers a thorough understanding of disease patterns and manifestations by including annotated pictures of leaves from a range of crops. Researchers, data scientists, and innovators who want to create specialized and efficient solutions for the agricultural problems the African continent faces will find this wealth of knowledge to be of immeasurable use.**
 
-1. Train (5709 images)
-2. Valid (1641 images)
-3. Test ( 826 images)
- 
-    """
+> **This "Afrocentric" dataset essentially serves as a testament to the understanding of the significance of region-specific data in promoting technological solutions that connect with the demands and complexities of Africa. Datasets of this kind open the way for more informed and contextually relevant developments as we navigate the complex convergence of technology and agriculture, ensuring that the advantages of the fourth industrial revolution are realized inclusively across various global landscapes.**
+
+### Plant Village
+The Plant Village dataset offers an extensive collection of 70,000 high-quality images, showcasing diseased and healthy plant leaves from nine different species.
+
+> **This dataset consists of 70,000 high-quality images of diseased and healthy plant leaves from 9 different species. Each species has 3 data splits (train, test, and validation), with consistent categories across all splits.**
+
+> **This dataset is ideal for machine learning researchers and practitioners working on plant disease detection and classification, as well as for agricultural experts seeking to improve plant health and crop yields.**
+
+> **The dataset is unique in its diversity, covering a wide range of plant species, diseases, and growth stages. With this dataset, we aim to accelerate research and development in the field of plant pathology and help farmers improve their crop health and productivity.**
+
+
+
+### About Us
+
+#### Ignatus Anim
+Ignatus is a tech enthusiast with a deep interest in machine learning, artificial intelligence, and software engineering. He has been instrumental in developing and training the machine learning models that power our app, as well as building the app itself. His dedication to optimizing algorithms and software architecture ensures that our app delivers accurate and reliable predictions.
+
+#### Yurlim Azindow
+Yurlim is our software development expert with a strong background in UI building and testing. She has been instrumental in designing and coding the app's user interfaces, ensuring a seamless and user-friendly experience. Her commitment to detail and usability makes our app accessible and easy to navigate for farmers of all tech levels.
+
+#### Joel Afeku Selorm Paa Kwesi Agbemenyo
+Joel brings his expertise in data analysis, UI building, and testing to the team. He has worked tirelessly on gathering and analyzing crop disease data, as well as designing intuitive interfaces for the app. Joel's organizational skills and testing efforts ensure that our app is both reliable and effective, meeting the needs of our users and the project's objectives.
+
+#### Mission
+Our mission is to empower farmers by providing them with a reliable, easy-to-use tool to identify and manage crop diseases. By leveraging the power of machine learning and image recognition, we aim to reduce crop losses, increase yields, and ultimately contribute to global food security.
+
+"""
 
 homeinfo = """
         Welcome to the AgriDetect App
@@ -51,12 +66,9 @@ homeinfo = """
 
 
     ### Get Started
-
-        Click on the **Disease Recognition** page in the sidebar to upload an image and 
-        experience the power of our Crop Disease Recognition system
+     Click on the Disease Recognition page in the sidebar to upload an image and experience the power of our Crop Disease Recognition system
 
     ### About Us
-
-        Learn more about the project, our team and our goals on the **About** page.
+    Learn more about the project, our team and our goals on the About page.
 
     """
